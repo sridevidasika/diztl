a09d8bbcc49806866170a03c4a38f228e03111e6
Sandeep Dasika <dasika.sandy@gmail.com>
2019-02-10 14:27:03 +0530

    Implementing search() in TrackerService

--- src/main/java/io/github/gravetii/tracker/TrackerService.java
+++ src/main/java/io/github/gravetii/tracker/TrackerService.java
@@ -1,10 +1,14 @@
 package io.github.gravetii.tracker;
 
+import io.github.gravetii.common.NodeConnection;
 import io.github.gravetii.generated.DiztilPojo;
 import io.github.gravetii.generated.TrackerServiceGrpc;
 import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 class TrackerService extends TrackerServiceGrpc.TrackerServiceImplBase {
   private static final Logger logger = LoggerFactory.getLogger(TrackerService.class.getCanonicalName());
@@ -25,7 +29,35 @@ class TrackerService extends TrackerServiceGrpc.TrackerServiceImplBase {
 
   @Override
   public void search(DiztilPojo.SearchRequest request, StreamObserver<DiztilPojo.SearchResponse> responseObserver) {
+    logger.info("Executing search request {}", request);
+    List<DiztilPojo.SearchResponse> responses = broadcast(request);
+    responses.forEach(responseObserver::onNext);
+    responseObserver.onCompleted();
+  }
 
+  private List<DiztilPojo.SearchResponse> broadcast(DiztilPojo.SearchRequest request) {
+    logger.info("Broadcasting search request to all active nodes - {}", request);
+    List<DiztilPojo.SearchResponse> result = new ArrayList<>();
+
+    for (DiztilPojo.Node node: nodeKeeper.getActiveNodes()) {
+      if (node.getIp().equals(request.getSource().getIp())) {
+        continue;
+      }
+
+      NodeConnection connection = nodeKeeper.getConnection(node);
+      DiztilPojo.SearchResponse response = connection.getStub().search(request);
+      if (response.getCount() > 0) {
+        DiztilPojo.SearchResponse finalResponse =
+                DiztilPojo.SearchResponse.newBuilder()
+                        .setCount(response.getCount())
+                        .addAllFiles(response.getFilesList())
+                        .setNode(node)
+                        .build();
+        result.add(finalResponse);
+      }
+    }
+
+    return result;
   }
 
 }
